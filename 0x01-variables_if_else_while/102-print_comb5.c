96ea12d374c7c1dcdfd7ecd9b9152ef25ba761b2
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-09-11 17:45:34 -0700

    Push

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,19 +11,21 @@ int main(void)
 
 	for (num1 = 0; num1 <= 99; num1++)
 	{
-		for (int num2 = num1 + 1; num2 <= 99; num2++) {
-            // Print the first two-digit number
-            putchar((num1 / 10) + '0');
-            putchar((num1 % 10) + '0');
+		int num2;
 
-            // Print a space to separate the numbers
-            putchar(' ');
+		for (num2 = num1 + 1; num2 <= 99; num2++)
+		{
+			// Print the first two-digit number
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			// Print a space to separate the number
+			putchar(' ');
 
-            // Print the second two-digit number
-            putchar((num2 / 10) + '0');
-            putchar((num2 % 10) + '0');
+           		 // Print the second two-digit number
+          	 	 putchar((num2 / 10) + '0');
+           		 putchar((num2 % 10) + '0');
 
-            // Print ", " after the combination if it's not the last one
+           	 // Print ", " after the combination if it's not the last one
             if (num1 != 98 || num2 != 99) {
                 putchar(',');
                 putchar(' ');
