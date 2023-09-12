7e18269e5e156b0a46f17cc53e4aa0dcbf514df7
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-09-12 15:50:51 -0700

    print_alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,5 @@
 #include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-/**
  * print_alphabet - Prints the lowercase alphabet followed by a newline.
  */
 void print_alphabet(void)
