7db22cb665677c1b052a4bf8e18dfbe48d213b0e
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-09-12 18:26:44 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,12 +2,12 @@
 
 /**
  * print_last_digit - Checks if a character is lowercase.
- * @c: The character to be checked.
+ * @n: The character to be checked.
  *
  * Return: 1 if 'c' is a lowercase letter, 0 otherwise.
  */
 
-int print_last_digit(int c)
+int print_last_digit(int n)
 {
 	int last_digit;
 
