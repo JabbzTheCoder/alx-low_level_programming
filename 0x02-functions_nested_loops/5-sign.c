46e405219d68c9075d807a52d04821b6e4381dbd
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-09-12 17:14:47 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - Checks if a character is lowercase.
+ * @n: The character to be checked.
+ *
+ * Return: 1 if 'c' is a lowercase letter, 0 otherwise.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar('1');
+		return (-1);
+	}
+}
