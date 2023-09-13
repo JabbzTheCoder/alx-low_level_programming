3bb3f6a7a9da7d2e4af610c5dea57dcbb0e90be6
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-09-13 12:29:12 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the times table
+ *
+ */
+void times_table(void)
+{
+	int i, j, result;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			result = i * j;
+			if (j > 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (result < 10)
+				{
+					_putchar(' ');
+				}
+			}
+			if (result < 10)
+			{
+				_putchar('0' + result);
+			}
+			else
+			{
+				_putchar('0' + (result / 10);
+				_putchar('0' + (result % 10));
+			}
+		}
+	}
+	_putchar('\n');
+}
