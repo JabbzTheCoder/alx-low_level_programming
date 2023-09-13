b220cab8859c40761f2646c9225b08931f7cfdb0
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-09-13 14:51:58 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Adds two intergers
+ * @a: Inerger 1
+ * @b: Interger 2
+ *
+ * Return: 1 if 'c' is a lowercase letter, 0 otherwise.
+ */
+
+int add(int a, int b)
+{
+	return (a+b);
+}
