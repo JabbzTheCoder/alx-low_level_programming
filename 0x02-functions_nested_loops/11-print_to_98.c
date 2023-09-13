15d215afbb9f07aac11842cd43900f21c117047c
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-09-13 16:26:06 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,81 +1,71 @@
 #include "main.h"
-/**
- * print_to_98 - Prints all natural numbers till 98
- * @n - strating n
- *
- */
 
+/**
+ * print_to_98 - Prints all natural numbers from n to 98
+ * @n: The starting number
+ */
 void print_to_98(int n)
 {
-	int i, ones, tens, hundreds;
-	
-	if (n > 98)
-	{
-		for (i = n ; i >= 98; i--)
-		{
-			if (i >= 100 && i < 1000)
-			{
-				hundreds = i / 100;
-				tens = (i / 10) % 10;
-				ones = i % 10;
-				_putchar(hundreds + '0');
-			}
-			else
-			{
-				tens = i / 10;
-				ones = i % 10;
-			}
-			_putchar(tens + '0');
-			_putchar(ones + '0');
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-	}
-	else
-	{
-		for (i = n; i < 99; i++)
-		{
-			if (i < 0)
-			{
-				_putchar('-');
-				
-				if ((-i) > 9)
-				{
-					tens = i / 10;
-					ones = i % 10;
-					_putchar(tens);
-					_putchar(ones);
-				}
-				else
-				{
-					_putchar(i + '0');
-				}
+    int i, tens, ones;
 
-			}
-			else
-			{
-				if (i > 9)
-				{
-					tens = i / 10;
-					ones = i % 10;
-					_putchar(tens);
-					_putchar(ones);
-                                }
-                                else
-                                {
-					_putchar(i + '0');
-                                }
-			}
-			if (i != 98)
-                        {
-				_putchar(',');
-				_putchar(' ');
-                        }
-		
-		}
-	}
+    if (n <= 98)
+    {
+        for (i = n; i <= 98; i++)
+        {
+            if (i < 0)
+            {
+                _putchar('-');
+                i = -i;
+            }
 
+            if (i > 9)
+            {
+                tens = i / 10;
+                ones = i % 10;
+                _putchar(tens + '0');
+                _putchar(ones + '0');
+            }
+            else
+            {
+                _putchar(i + '0');
+            }
+
+            if (i != 98)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+    }
+    else
+    {
+        for (i = n; i >= 98; i--)
+        {
+            if (i < 0)
+            {
+                _putchar('-');
+                i = -i;
+            }
+
+            if (i > 9)
+            {
+                tens = i / 10;
+                ones = i % 10;
+                _putchar(tens + '0');
+                _putchar(ones + '0');
+            }
+            else
+            {
+                _putchar(i + '0');
+            }
+
+            if (i != 98)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+    }
+    _putchar('\n');
 }
+
