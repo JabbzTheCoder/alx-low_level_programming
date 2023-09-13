fc7e62344f00b061f54950a514849fd9d99d6fec
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-09-13 12:51:20 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,9 +9,11 @@ void times_table(void)
 	int i, j, result;
 
 	for (i = 0; i <= 9; i++)
-	{
+	{	
 		for (j = 0; j <= 9; j++)
 		{
+			result = i * j;
+
 			if (result < 10)
 			{
 				_putchar('0' + result);
