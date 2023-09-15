7f22f7df9f909dedf5da8a0363a28a56c27018d0
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-09-14 19:01:37 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - check the code
+ *@n: len of line
+ */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		int i;
+
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
