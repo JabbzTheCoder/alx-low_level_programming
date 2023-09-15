f325f0b53f6e2dd1064bff5ed8dae55d48a7c028
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-09-14 18:46:15 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * more_numbers - check the code
+ *
+ */
+
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		int j;
+
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
+			else
+			{
+				_putchar(j + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
