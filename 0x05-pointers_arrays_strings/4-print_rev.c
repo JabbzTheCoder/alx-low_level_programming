4b39a0959a06bf3d8add7276e89bc7214220e09f
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-09-18 12:08:47 -0700

    I can only go one way. I've not got a reverse gear

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev  - check the code
+ *
+ *@s: string to be printed.
+ */
+void print_rev(char *s)
+{
+	int len;
+       
+	len = strlen(s) - 1;
+	while (len >= 0)
+	{
+		_putchar(s[len]);
+		len--;
+	}
+}
