8e9b76c6225802a2609661fd00a5279296353642
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-09-18 16:57:35 -0700

    char *_strcpy(char *dest, char *src)

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * *_strcpy - check the code
+ *
+ * @dest:destination
+ * @src:source
+ *Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (*dest);
+}
