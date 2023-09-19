4336b3574421cd77616c014552458cccdd5ddd81
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-09-18 17:32:52 -0700

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _atoi - check the code
+ *
+ * @s:source
+ *Return: dest
+ */
+
+int _atoi(char *s);
+{
+	return (atoi(s));
+}
