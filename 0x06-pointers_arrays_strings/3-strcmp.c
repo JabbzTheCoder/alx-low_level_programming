c626e6849e668daf9cc73a73dfe62f4febec4155
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-09-21 14:52:51 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcmp - check the code
+ * @s1: destination
+ * @s2: source
+ * Return: Always 0.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
