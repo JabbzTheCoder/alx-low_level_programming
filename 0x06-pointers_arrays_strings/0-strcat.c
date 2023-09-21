162458db84cc4483bba6fe2323b069f79c74a60b
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-09-21 14:13:12 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * *_strncat - check the code
+ *
+ * @dest:destination
+ * @src:source
+ * 
+ * Return: Always 0.
+ */
+
+char *_strncat(char *dest, char *src)
+{
+	strcat(dest, src);
+
+	return (dest);
+}
