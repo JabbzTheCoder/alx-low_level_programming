b97688223b666bda9d48a997f7dddb78ea027340
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-09-22 16:24:20 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strchr - locates a character
+ *
+ * @s: s
+ * @c: c 
+ * Return: Nothing.
+ */
+
+char *_strchr(s, c)
+{
+
+	return (strchr(s, c));
+}
