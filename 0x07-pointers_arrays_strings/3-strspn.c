e82c842d35794ca06c33b61404f63119cc775501
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-09-22 16:48:50 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strspn - memory set
+ *
+ * @s : s
+ * @accept: b
+ * Return: Nothing.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	return (strspn(s, accept));
+}
