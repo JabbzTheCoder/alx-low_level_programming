a0bda62ae9f4d68cb3d6697a55bc9d956aaa3c09
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-09-22 17:03:47 -0700

    4. strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strpbrk - memory set
+ *
+ * @s : s
+ * @accept: b
+ * Return: Nothing.
+ */
+
+unsigned int _strpbrk(char *s, char *accept)
+{
+	return (strpbrk(s, accept));
+}
