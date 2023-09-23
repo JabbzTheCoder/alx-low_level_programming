a2fa324f60a3878a5e03f510834e8e3b70b617be
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-09-22 17:33:17 -0700

    5. strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strstr - memory set
+ *
+ * @haystack: hs
+ * @needle: b
+ * Return: Nothing.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	return (strstr(s, accept));
+}
