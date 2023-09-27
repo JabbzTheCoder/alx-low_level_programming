47a1c3caccc834aa5deb1be5c5edeeb5af5b3343
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-09-27 13:37:23 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,17 +1,27 @@
 #include "main.h"
 
+int is_prime_helper(int n, int divisor);
 /**
  * is_prime_number - check the code
  * @n: num
  * Return: Always 0.
  */
-
 int is_prime_number(int n)
 {
 	if (n <= 1)
 	{
 		return (0);
 	}
+	return (is_prime_helper(n, 2));
+}
+/**
+ * is_prime_helper - check the code
+ * @n: num
+ * @divisor: divisor
+ * Return: Always 0.
+ */
+int is_prime_helper(int n, int divisor)
+{
 	if (divisor * divisor > n)
 	{
 		return (1);
@@ -20,5 +30,5 @@ int is_prime_number(int n)
 	{
 		return (0);
 	}
-	return (is_prime_number(n, divisor + 1));
+	return (is_prime_helper(n, divisor + 1));
 }
