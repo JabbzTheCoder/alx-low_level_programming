38e770ef8cff624452156da314c033070a7b37a2
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-10-05 05:08:04 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * malloc_checked - check the code
+ * @b: no of bytes
+ * Return: Always 0.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (*ptr == NULL)
+	{
+		exit(98);
+	}
+
+	return (ptr);
+}
