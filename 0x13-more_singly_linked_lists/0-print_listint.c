6993bbb80cf891114b336404eed65c7cfe0857e4
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-10-23 11:20:04 -0700

    push

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - check the code
+ * @h : list
+ * Return: Always 0.
+ */
+size_t print_listint(const listint_t *h)
+{
+	if (h == NULL)
+	{
+		return (0);
+	}
+
+	size_t n;
+	listint_t *nxtNode;
+
+	nxtNode = h;
+	n = 0;
+
+	while (nxtNode != NULL)
+	{
+		printf("%d\n", nxtNode->n);
+		n++;
+		nxtNode = nxtNode->next; 
+	}
+
+	return (n);
+}
