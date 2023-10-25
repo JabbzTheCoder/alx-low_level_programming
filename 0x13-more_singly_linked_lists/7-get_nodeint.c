e7253261e973c5ff76a8082d77d164c5ed110b81
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-10-25 15:13:22 -0700

    push

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,16 +10,16 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
-	listint_t *temp = head;
-	
-	while (temp && i < index)
+	listint_t *tmp = head;
+
+	while (tmp && i < index)
 	{
 		temp = temp->next;
 		i++;
 	}
-	if (temp)
+	if (tmp)
 	{
-		return (temp);
+		return (tmp);
 	}
 	else
 	{
