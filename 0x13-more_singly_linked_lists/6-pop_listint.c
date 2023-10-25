e751f905037464e9603730ac92eee58aad796316
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-10-25 15:27:46 -0700

    push

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,15 +11,14 @@ int pop_listint(listint_t **head)
 {
 	listint_t *tmp;
 	int n;
-	
+
 	if (!head || !*head)
 		return (0);
-	
+
 	n = (*head)->n;
 	tmp = (*head)->next;
 	free(*head);
 	*head = tmp;
-	
 	return (n);
 
 }
