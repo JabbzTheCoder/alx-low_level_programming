fa039adb126e8bdb6531a57228ca6c0eef70017f
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-10-30 14:31:50 -0700

    push

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,66 +2,71 @@
 #include <stdio.h>
 
 /**
- * error_file - Check if files can be opened successfully.
- * @source_fd: File descriptor for the source file.
- * @dest_fd: File descriptor for the destination file.
- * @argv: Arguments vector.
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
  */
-void error_file(int source_fd, int dest_fd, char *argv) {
-    if (source_fd == -1) {
-        dprintf(STDERR_FILENO, "Error: Unable to read from file %s\n", argv[1]);
-        exit(98);
-    }
-    if (dest_fd == -1) {
-        dprintf(STDERR_FILENO, "Error: Unable to write to file %s\n", argv[2]);
-        exit(99);
-    }
+void error_file(int file_from, int file_to, char *argv[])
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 }
 
 /**
- * main - Copy the contents of one file to another.
- * @argc: Number of arguments.
- * @argv: Arguments vector.
- * Return: 0 on success, other values on error.
+ * main - check the code for Holberton School students.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ * Return: Always 0.
  */
-int main(int argc, char *argv) {
-    int source_fd, dest_fd, err_close;
-    ssize_t nchars, nwr;
-    char buffer[1024];
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, err_close;
+	ssize_t nchars, nwr;
+	char buf[1024];
 
-    if (argc != 3) {
-        dprintf(STDERR_FILENO, "Usage: cp source_file destination_file\n");
-        return 97;
-    }
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
 
-    source_fd = open(argv[1], O_RDONLY);
-    dest_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-    error_file(source_fd, dest_fd, argv);
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
 
-    while ((nchars = read(source_fd, buffer, sizeof(buffer)) > 0) {
-        nwr = write(dest_fd, buffer, nchars);
-        if (nwr == -1) {
-            dprintf(STDERR_FILENO, "Error: Unable to write to file %s\n", argv[2]);
-            exit(99);
-        }
-    }
+	nchars = 1024;
+	while (nchars == 1024)
+	{
+		nchars = read(file_from, buf, 1024);
+		if (nchars == -1)
+			error_file(-1, 0, argv);
+		nwr = write(file_to, buf, nchars);
+		if (nwr == -1)
+			error_file(0, -1, argv);
+	}
 
-    if (nchars == -1) {
-        dprintf(STDERR_FILENO, "Error: Unable to read from file %s\n", argv[1]);
-        exit(98);
-    }
+	err_close = close(file_from);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
 
-    err_close = close(source_fd);
-    if (err_close == -1) {
-        dprintf(STDERR_FILENO, "Error: Unable to close source file descriptor\n");
-        exit(100);
-    }
-
-    err_close = close(dest_fd);
-    if (err_close == -1) {
-        dprintf(STDERR_FILENO, "Error: Unable to close destination file descriptor\n");
-        exit(100);
-    }
-
-    return 0;
+	err_close = close(file_to);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	return (0);
 }
