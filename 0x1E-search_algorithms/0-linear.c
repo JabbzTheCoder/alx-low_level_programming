163b43cec79d63989c6009f8ad462ccc279a2ce4
JabbzTheCoder <jabbzthecoder@gmail.com>
2024-04-10 22:52:23 +0200

    push

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -8,21 +8,20 @@
  * Return: The first index where value is located, or -1 if not found
  *         or if array is NULL.
  */
-int linear_search(const int *array, size_t size, int value)
+int linear_search(int *array, size_t size, int value)
 {
-    size_t i;
-    
-    if (array == NULL)  // Check for null array
-        return -1;
-    
-    for (i = 0; i < size; i++)
-    {
-        printf("Value checked array[%zu] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return i;  // Return index if value found
-    }
+	size_t i;
 
-    return -1;  // Value not found
+	if (array == NULL)
+		return (-1);
 
-    
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
 }
+
